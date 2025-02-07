8b7f68556755115d7e474f550aca6ce62a30a6ed
Ajay Solanki <ajaysolanki.pvt@gmail.com>
2025-02-07 16:31:57 +0530

    change img

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$3wix$s!(l#9cms^&6(@o9wbm(x+)1lf69ri8ax7sp6cx5g-ij'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
